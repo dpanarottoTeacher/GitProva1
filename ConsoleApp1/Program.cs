bfe8e4d95e6386154031cee1827cb0e230eba69d
Enrica Taffurelli <enrica.taffurelli@gmail.com>
2023-11-22 17:57:14 +0100

    Modifica 2

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!!!!");
 Console.WriteLine("Modifica Denise1");
+Console.WriteLine("Modifica Enrica 1");
